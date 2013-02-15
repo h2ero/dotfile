89c6316dc6199b3b5ef77f156c52a1b93147e576
h2ero <122750707@qq.com>
2013-02-15 20:27:29 +0800

    add new config

--- init.sh
+++ init.sh
@@ -1,5 +1,11 @@
+# vim
 mv ~/.vim ~/.vim_backup
 mv ~/.vimrc ~/.vimrc_backup
 ln -s $(pwd)/.vimrc ~/.vimrc
 ln -s $(pwd)/.vim ~/.vim
 
+# pentadactyl
+mv ~/.pentadactyl ~/.pentadactyl_backup
+mv ~/.pentadactylrc ~/.pentadactylrc_backup
+ln -s $(pwd)/.pentadactyl ~/.pentadactyl
+ln -s $(pwd)/.pentadactylrc ~/.pentadactylrc
