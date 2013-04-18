aceccd1ecf5a5594d31585631885543a0b99814d
h2ero <122750707@qq.com>
2013-04-18 15:48:49 +0800

    checkout git-init.sh and init.sh form master

---
+++ init.sh
@@ -0,0 +1,25 @@
+# vim
+mv ~/.vim ~/.vim_backup
+mv ~/.vimrc ~/.vimrc_backup
+ln -s $(pwd)/.vimrc ~/.vimrc
+ln -s $(pwd)/.vim ~/.vim
+
+# pentadactyl
+mv ~/.pentadactyl ~/.pentadactyl_backup
+mv ~/.pentadactylrc ~/.pentadactylrc_backup
+ln -s $(pwd)/.pentadactyl ~/.pentadactyl
+ln -s $(pwd)/.pentadactylrc ~/.pentadactylrc
+
+# awesome WM
+mv ~/.config/awesome ~/.config/awesome_backup
+ln -s $(pwd)/.config/awesome ~/.config/awesome
+
+# oh my zsh
+sudo apt-get install zsh
+wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+chsh -s `which zsh`
+
+curl http://betterthangrep.com/ack-standalone > ~/bin/ack 
+chmod 755 ~/bin/ack
+
+
