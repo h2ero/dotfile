c89d1f2609c36e1d02e6fce5d125f5c176678088
h2ero <122750707@qq.com>
2013-11-23 09:37:18 +0800

    added flymake cursor

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -47,3 +47,21 @@
                             (files ("*.jpg" "*.png" "*.zip" "*~"))))
 
 (global-set-key (kbd "M-x") 'smex)
+
+;check php
+(require 'flymake)
+
+(defun flymake-php-init ()
+  "Use php to check the syntax of the current file."
+  (let* ((temp (flymake-init-create-temp-buffer-copy
+                 'flymake-create-temp-inplace))
+         (local (file-relative-name temp
+                                    (file-name-directory buffer-file-name))))
+    (list "php" (list "-f" local "-l"))))
+
+(add-to-list 'flymake-err-line-patterns
+             '("\\(Parse\\|Fatal\\) error: +\\(.*?\\)
+               in \\(.*?\\) on line \\([0-9]+\\)$" 3 4 nil 2))
+
+(add-to-list 'flymake-allowed-file-name-masks
+             '("\\.php$" flymake-php-init))
