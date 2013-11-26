a8cde095d25dc9bf392c06c10c224ad57b5843c2
h2ero <122750707@qq.com>
2013-11-26 09:38:53 +0800

    update

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -20,19 +20,19 @@
 (add-to-list 'custom-theme-load-path "~/.emacs.d/elpa/monokai-theme-0.0.10")
 (load-theme 'monokai t)
 
-(add-to-list 'load-path "~/.emacs.d/elpa/php-mode-1.5.0") 
+(add-to-list 'load-path "~/.emacs.d/el/php-mode") 
 (require 'php-mode)
-(add-hook 'php-mode-hook (lambda ()
-    (defun ywb-php-lineup-arglist-intro (langelem)
-      (save-excursion
-        (goto-char (cdr langelem))
-        (vector (+ (current-column) c-basic-offset))))
-    (defun ywb-php-lineup-arglist-close (langelem)
-      (save-excursion
-        (goto-char (cdr langelem))
-        (vector (current-column))))
-    (c-set-offset 'arglist-intro 'ywb-php-lineup-arglist-intro)
-    (c-set-offset 'arglist-close 'ywb-php-lineup-arglist-close)))
+; (add-hook 'php-mode-hook (lambda ()
+;     (defun ywb-php-lineup-arglist-intro (langelem)
+;       (save-excursion
+;         (goto-char (cdr langelem))
+;         (vector (+ (current-column) c-basic-offset))))
+;     (defun ywb-php-lineup-arglist-close (langelem)
+;       (save-excursion
+;         (goto-char (cdr langelem))
+;         (vector (current-column))))
+;     (c-set-offset 'arglist-intro 'ywb-php-lineup-arglist-intro)
+;     (c-set-offset 'arglist-close 'ywb-php-lineup-arglist-close)))
 
 
 (add-to-list 'load-path "~/.emacs.d/el/evil") 
